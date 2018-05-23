70e12b6cbf67f0c38fae5d7ad82b3b8f0ccc76da
Joel Pagliuca <Joel.Pagliuca@cba.com.au>
2018-05-23 19:55:13 +1000

    now using actual typescript

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as url from "url";
+
+let mainWindow: BrowserWindow;
+
+/**
+ * Setup a new window and load the index page
+ */
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+  });
+  
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "../index.html"),
+    protocol: "file:",
+    slashes: true,
+  }));
+
+  mainWindow.webContents.openDevTools();
+
+  // handle closing the window
+  mainWindow.on("close", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
