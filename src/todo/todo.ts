59d9b4a2da85307c77b931d9dc304dccf2fed047
JoelPagliuca <joelpagliuca@hotmail.com>
2018-06-21 19:38:33 +1000

    got todo items toggling properly with strikethrough

--- src/todo/todo.ts
+++ src/todo/todo.ts
@@ -1,14 +1,53 @@
 import { ipcRenderer } from "electron";
 
 function markDone(todoId:string) {
-	let todoItem = document.getElementById(todoId);
-	todoItem.classList.toggle('todo-done');
-	todoItem.classList.toggle('has-text-success');
+  let todoItem = document.getElementById(todoId);
+  todoItem.classList.toggle('todo-done');
+  todoItem.classList.toggle('has-text-success');
 }
 
-let todoList: string
+class TodoItem {
+  text: string;
 
-todoList = ipcRenderer.sendSync("load-file");
+  constructor(text: string) {
+    this.text = text;
+  }
 
-let todoItems = todoList.split(',');
-console.log(todoItems);
+  toNode(): Node {
+    let node = document.createElement('div');
+    let htmlText = `<a class="panel-block todo-item">
+      <span>${this.text}</span>
+    </a>`
+    node.innerHTML = htmlText.trim();
+    return node.firstChild;
+  }
+}
+
+function setOnClickListeners() {
+  const toggleStrikethrough = (event: MouseEvent) => { 
+    let target: HTMLLinkElement;
+    target = <HTMLLinkElement> event.target;
+    target.classList.toggle('todo-done');
+   };
+
+  let todoItems = document.getElementsByClassName('todo-item');
+  for (let i = 0; i < todoItems.length; i++) {
+    const todoItem = todoItems.item(i);
+    todoItem.addEventListener("click", toggleStrikethrough);
+  }
+}
+
+function getInitialList() {
+  let todoList: string
+  todoList = ipcRenderer.sendSync("load-file");
+
+  let todoItems = todoList.split(',').map((x: string) => {return new TodoItem(x)});
+  let todoPanel = document.getElementById('todo-panel');
+  todoItems.forEach((item: TodoItem) => {
+    todoPanel.appendChild(item.toNode());
+  });
+
+  setOnClickListeners();
+}
+
+getInitialList();
